9fbacc2cf8debb64173728bfe4d4f7e0cc879832
Vansh Motla <vansh.motla07@gmail.com>
2026-04-05 00:09:41 +0530

    Day 1 - journey begins

---
+++ month-1/day1.py
@@ -0,0 +1,7 @@
+name = input("Enter your name: ")
+goal = input("Enter your 6 month goal ")
+months =6
+print(f"\nName: {name}")
+print(f"Goal: {goal}")
+print(f"time to achieve it {months} months")
+print(f"\nLets go {name}. lets do it")
